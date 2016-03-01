fdc0b6a84d7177023d574be877cfc825c8b462f8
hanneswestberg <hannesve@kth.se>
2016-03-01 12:35:48 +0100

    UI and shitz
    yes

---
+++ Scripts/UIScriptManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIScriptManager : MonoBehaviour {
+
+	public Text inputFieldUserName;
+	public Text inputFieldPassword;
+
+	void Start(){
+		
+	}
+
+
+	public void InputFieldChangeUserName(){
+		this.GetComponent<WWWFormUserLogIn>().UpdateUserName(inputFieldUserName.text);
+	}
+
+	public void InputFieldChangePassword(){
+		this.GetComponent<WWWFormUserLogIn>().UpdatePassword(inputFieldPassword.text);
+	}
+
+	public void ButtonLogIn(){
+		this.GetComponent<WWWFormUserLogIn>().SendLogInInfo();
+	}
+}
---
+++ Scripts/WWWFormUserLogIn.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WWWFormUserLogIn : MonoBehaviour {
+
+	string database_url = "http://xml.csc.kth.se/~thomasvp/DM2517/kex/index.php";
+	string userName = "";
+	string userPassword = "";
+
+	public void UpdatePassword (string value) {
+		userPassword = value;
+	}
+
+	public void UpdateUserName (string value) {
+		userName = value;
+	}
+
+	public IEnumerator SendLogInInfo(){
+
+		WWWForm form = new WWWForm();
+
+		form.AddField("userName", userName);
+		form.AddField("userPassword", userPassword); //EJ KRYPTERAD
+
+		Debug.Log(form.ToString());
+
+		WWW download = new WWW(database_url, form);
+
+		yield return download;
+
+		if(!string.IsNullOrEmpty(download.error)){
+			print("Error downloading: " + download.error);
+		}else{
+			Debug.Log(download.text);
+		}
+	}
+}
